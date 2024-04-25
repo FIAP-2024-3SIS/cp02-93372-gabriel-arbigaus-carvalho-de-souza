feabb23f9343603d4a1673f30545bfd3e86b34a8
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-24 22:47:34 -0300

    subindo check angular

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AnimalPageComponent } from "./animal-page/animal-page.component";
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, AnimalPageComponent]
+})
+export class AppComponent {
+  title = 'my-app';
+}
+
+
